5f7dc441fad3a76e6e926e617af5d3b24a485d26
aknafu12 <kgebre2021@gmail.com>
2024-06-03 16:55:45 +0300

    initial comit

---
+++ image_classification.py
@@ -0,0 +1,14 @@
+#
+# Importing Library and Data
+import pandas as pd
+import matplotlib.pyplot as plt
+import torch
+import torch.nn.functional as F
+import torchvision
+import torchvision.transforms as transforms
+
+from torch.utils.data import Dataset, DataLoader
+from sklearn.model_selection import train_test_split
+import os
+os.getcwd()
+
