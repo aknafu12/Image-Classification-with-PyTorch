f7c6b5333c406564ee1a287ed1993c043ef9c059
aknafu12 <kgebre2021@gmail.com>
2024-06-03 17:15:27 +0300

    added print propertiy

--- image_classification.py
+++ image_classification.py
@@ -12,7 +12,19 @@ from sklearn.model_selection import train_test_split
 import os
 os.getcwd()
 
-
+#
 labels = pd.read_csv(r'dataset/aerial-cactus-identification/train.csv')
 submission = pd.read_csv(r'dataset/aerial-cactus-identification/sample_submission.csv')
+# print top most and bottom
+print(labels.head())
+print(labels.tail())
+# value count
+print(labels['has_cactus'].value_counts())
+# visulize using pie chart
+label = 'Has Cactus', 'Hasn\'t Cactus'
+plt.figure(figsize = (8,8))
+plt.pie(labels.groupby('has_cactus').size(), labels = label, autopct='%1.1f%%', shadow=True, startangle=90)
+plt.show()
 
+
+# Image Pre-processing
