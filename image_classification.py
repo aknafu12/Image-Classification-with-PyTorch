97cb1453b4a01d05d04093bfbd91d3b34eb3a123
aknafu12 <kgebre2021@gmail.com>
2024-06-04 09:59:50 +0300

    Add imshow function  normalization for displaying images

--- image_classification.py
+++ image_classification.py
@@ -1,6 +1,7 @@
 #
 # Importing Library and Data
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.image as img
 import torch
@@ -45,4 +46,28 @@ fig,ax = plt.subplots(1,5,figsize = (15,3))
 for i,idx in enumerate(labels[labels['has_cactus'] == 0]['id'][:5]):
     path = os.path.join(train_path,idx)
     ax[i].imshow(img.imread(path))
+
+# standardize the image by defined mean and standard deviation
+
+def imshow(image, ax=None, title=None, normalize=True):
+    if ax is None:
+        fig, ax = plt.subplots()
+    image = image.numpy().transpose((1, 2, 0))
+
+    if normalize:
+        mean = np.array([0.485, 0.456, 0.406])
+        std = np.array([0.229, 0.224, 0.225])
+        image = std * image + mean
+        image = np.clip(image, 0, 1)
+
+    ax.imshow(image)
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    ax.spines['left'].set_visible(False)
+    ax.spines['bottom'].set_visible(False)
+    ax.tick_params(axis='both', length=0)
+    ax.set_xticklabels('')
+    ax.set_yticklabels('')
+
+    return ax
 
