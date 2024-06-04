da4110694088b164b2d79151cec807ecdf562272
aknafu12 <kgebre2021@gmail.com>
2024-06-04 09:46:48 +0300

    Visualize last 5 cactus images

--- image_classification.py
+++ image_classification.py
@@ -2,6 +2,7 @@
 # Importing Library and Data
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.image as img
 import torch
 import torch.nn.functional as F
 import torchvision
@@ -12,9 +13,14 @@ from sklearn.model_selection import train_test_split
 import os
 os.getcwd()
 
+
 #
 labels = pd.read_csv(r'dataset/aerial-cactus-identification/train.csv')
 submission = pd.read_csv(r'dataset/aerial-cactus-identification/sample_submission.csv')
+
+train_path =r'dataset/aerial-cactus-identification/train'
+test_path =r'dataset/aerial-cactus-identification/test'
+
 # print top most and bottom
 print(labels.head())
 print(labels.tail())
@@ -28,3 +34,15 @@ plt.show()
 
 
 # Image Pre-processing
+fig,ax = plt.subplots(1,5,figsize = (15,3))
+
+for i,idx in enumerate(labels[labels['has_cactus'] == 1]['id'][-5:]):
+    path = os.path.join(train_path,idx)
+    ax[i].imshow(img.imread(path))
+
+
+fig,ax = plt.subplots(1,5,figsize = (15,3))
+for i,idx in enumerate(labels[labels['has_cactus'] == 0]['id'][:5]):
+    path = os.path.join(train_path,idx)
+    ax[i].imshow(img.imread(path))
+
